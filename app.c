ed6cf3813dce27e12526e1b5618df06c662e5ef3
unknown <akderehuseyin3@gmail.com>
2024-03-30 03:06:15 +0300

    hard reset sonrası

---
+++ app.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+    int h,r;
+    float pi,hacim;
+    pi=3.1419;
+
+	printf("koninin yukseklini giriniz cm ");
+	scanf("%d",&h);
+	printf("koninin yaricapini giriniz cm ");
+	scanf("%d",&r);
+    
+    hacim=(pi*r*r*h)/3.0;
+	printf("hacim = %f cm kuptur",hacim);
+	
+	
+	
+	return 0;
+}
+
+//hard reset sonrasý yapýlan deðiþiklik
